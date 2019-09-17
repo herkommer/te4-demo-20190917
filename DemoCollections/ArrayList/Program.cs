6fae15122134013c526086e225e525bdbdc45012
herkommer <m@herkommer.co.uk>
2019-09-17 11:05:10 +0200

    Extracted code to avoid DRY

--- DemoCollections/ArrayList/Program.cs
+++ DemoCollections/ArrayList/Program.cs
@@ -17,15 +17,26 @@ namespace ArrayList
             MyList.Add("bbbb");
             MyList.Add("cc");
 
-            Console.WriteLine("Det finns {0} värden i listan",MyList.Count);
+            Console.WriteLine("Det finns {0} värden i listan", MyList.Count);
+            PrintArrayListValues(MyList);
 
+            MyList.Reverse();
+            PrintArrayListValues(MyList);
+
+
+        }
+
+        //DRY - Dont Repeat Yourself
+        //Vi extraherar kod till en metod istället
+        //så vi kan anropa och återanvända koden
+        private static void PrintArrayListValues(System.Collections.ArrayList MyList)
+        {
             foreach (string s in MyList)
             {
-                Console.WriteLine("[{2}] Value: {0} ({1})", s, 
-                                                            s.Length, 
+                Console.WriteLine("[{2}] Value: {0} ({1})", s,
+                                                            s.Length,
                                                             MyList.IndexOf(s));
             }
-
         }
     }
 }
